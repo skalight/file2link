90fc225f53f01a6edc73ab1a168cd9ad757bc5cd
haashim0 <103762873+haashim0@users.noreply.github.com>
2022-12-04 06:50:31 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -138,8 +138,8 @@ async def private_receive_handler(c: Client, m: Message):
         await c.send_message(chat_id=Var.BIN_CHANNEL, text=f"Gᴏᴛ FʟᴏᴏᴅWᴀɪᴛ ᴏғ {str(e.x)}s from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**𝚄𝚜𝚎𝚛 𝙸𝙳 :** `{str(m.from_user.id)}`", disable_web_page_preview=True, parse_mode="Markdown")
 
 
-@StreamBot.on_message(filters.channel & ~filters.group & (filters.document | filters.video | filters.photo) & ~filters.edited & ~filters.forwarded, group=-1)
-async def editing(bot, message, broadcast):
+@StreamBot.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
+async def editing(bot, broadcast, message):
     if MY_PASS:
         check_pass = await pass_db.get_user_pass(broadcast.chat.id)
         if check_pass == None:
