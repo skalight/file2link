c7815a9383c2b42606c10881484418d4b7b951c0
haashim0 <103762873+haashim0@users.noreply.github.com>
2022-12-04 11:10:39 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -22,11 +22,11 @@ class Var(object):
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5291606032").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = filetolinktamildub
+    APP_NAME = false
     OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))
+        APP_NAME = str(getenv('APP_NAME', 'filetodub'))
     
     else:
         ON_HEROKU = False
