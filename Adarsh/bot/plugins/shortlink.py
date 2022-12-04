4579f18c7033c3779cf3e8b3078ad87b9c7eb2ca
haashim0 <103762873+haashim0@users.noreply.github.com>
2022-12-04 14:02:29 +0530

    Create shortlink.py

---
+++ Adarsh/bot/plugins/shortlink.py
@@ -0,0 +1,24 @@
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+    url = f'https://shorturllink.in/api'
+    params = {'api': SHORTENER_API,
+              'url': link,
+              }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://shorturllink.in/api?api={SHORTENER_API}&link={link}'
+
+
+    except Exception as e:
+        logger.error(e)
+        return f'https://shorturllink.in/api?api={SHORTENER_API}&link={link}'
