80b5b99feaa845ba881a5450e2928480014ee1ef
haashim0 <103762873+haashim0@users.noreply.github.com>
2022-12-05 17:19:21 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -12,7 +12,7 @@ class Var(object):
     API_ID = int(getenv('API_ID', '14505719'))
     API_HASH = str(getenv('API_HASH', '620f0a2aa2cd1474a4953619b3e3643d'))
     BOT_TOKEN = str(getenv('BOT_TOKEN', '5397695914:AAGOQT0lD37ilkl7ihAovNEvee8XeZQQhf8'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'filetolinkbot'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'filetoink'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001720440096'))
@@ -22,11 +22,14 @@ class Var(object):
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5291606032").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
+    SHORTENER_API = environ.get('SHORTENER_API', '4fbe384f585aa6c0df736bd9a859bfc3b9216fee')
+    SHORTENER_WEBSITE = environ.get('SHORTENER_WEBSITE', 'tnlink.in')
+
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME', 'filetodub'))
+        APP_NAME = str(getenv('APP_NAME', 'filetoink'))
     
     else:
         ON_HEROKU = False
@@ -37,5 +40,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://Haashim:Haashim@mfile0.t9hxg.mongodb.net/?retryWrites=true&w=majority"))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "@TamilMob_LinkZz"))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
