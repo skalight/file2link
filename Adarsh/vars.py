a2a8f74fabe5d2169ac58132f64de2c33c71f8d6
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-04-15 14:18:00 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,16 +11,16 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '14505719'))
     API_HASH = str(getenv('API_HASH', '620f0a2aa2cd1474a4953619b3e3643d'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '5397695914:AAGOQT0lD37ilkl7ihAovNEvee8XeZQQhf8'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'filetoink'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6126205588:AAEpoJttxR56CI9ubvy7P6O89ENjD7aGqWU'))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'filetolinkda'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001720440096'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001659362808'))
     PORT = int(getenv('PORT', 8080))
     CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "BOTTOM")
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5291606032").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1893496887").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     SHORTENER_API = str(getenv('SHORTENER_API', '4fbe384f585aa6c0df736bd9a859bfc3b9216fee'))
     SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'tnlink.in'))
@@ -29,7 +29,7 @@ class Var(object):
     OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME', 'filetoink'))
+        APP_NAME = str(getenv('APP_NAME', 'filetolinkda'))
     
     else:
         ON_HEROKU = False
@@ -40,5 +40,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://Haashim:Haashim@mfile0.t9hxg.mongodb.net/?retryWrites=true&w=majority"))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "@TamilMob_LinkZz"))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "@tamilanmovieschat"))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
